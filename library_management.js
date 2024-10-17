19e27dba1ffd37ba78076d201dad6a70dae9f3bc
Laura Comar <lautmcomar@gmail.com>
2024-10-17 10:46:31 -0400

    Create Patron class to borrow and return books

--- library_management.js
+++ library_management.js
@@ -36,3 +36,26 @@ class Section { // store books in array
         return this.books.filter(book => book.isAvailable).length;
     } // use filter to get available books
 }
+
+// Task 3: Create a Patron Class
+class Patron {
+    constructor(name) {
+        this.name = name;
+        this.borrowedBooks = []; // store array of books borrowed
+    }
+    borrowedBook(book) { //  Allows the patron to borrow a book if it is available and updates the book’s status
+        if (book.isAvailable) { // check if book is available
+            this.borrowedBooks.push(book); // add book to borrowed
+            book.isAvailable = false;
+            console.log(`Book is borrowed`);
+        } else { console.log(`Book is not available`) }
+    }
+    returnBook(book) { // Allows the patron to return a borrowed book, making it available again
+        const bookIndex = this.borrowedBooks.indexOf(book); // find book
+        if (bookIndex > -1) { // check if book is  borrowed
+            this.borrowedBooks.splice(bookIndex, 1); // remove from borrowed 
+            book.isAvailable = true; // update status to available
+            console.log(`Book is returned`);
+        } else { console.log(`Book was not borrowed`) }
+    }
+}
